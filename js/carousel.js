d3f299f091acef6c3ff5addf2582fdef4ff359d6
catropio <zagloon@gmail.com>
2017-11-28 22:49:21 -0300

    Subida v3 web

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
   $('.customer-logos').slick({
-    slidesToShow: 6,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
+  });
+});
+
+$(document).ready(function(){
+  $('.certification-logos').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
     autoplaySpeed: 2000,
     arrows: false,
     dots: false,
